4b781385dbcbf2b51fe2685a6c88d3b08fa87a80
rongbang <rongbang.ye@gmail.com>
2021-01-25 00:44:26 -0800

    added new project

--- js/index.js
+++ js/index.js
@@ -1,27 +1,38 @@
-console.log("welcome");
-const cover = document.querySelector(".cover");
-const logo = document.querySelector(".logo");
-const theme = document.querySelector("#theme");
-const github = document.querySelector("#github");
-const linkedin = document.querySelector("#linkedin");
-const email = document.querySelector("#email");
-const big = document.querySelector(".big");
-const bigAboutMe = document.querySelector(".big-aboutme");
+console.log('welcome');
+const cover = document.querySelector('.cover');
+const cover2 = document.querySelector('.cover2');
+const cover3 = document.querySelector('.cover3');
+const logo = document.querySelector('.logo');
+const theme = document.querySelector('#theme');
+const github = document.querySelector('#github');
+const linkedin = document.querySelector('#linkedin');
+const email = document.querySelector('#email');
+const big = document.querySelector('.big');
+const bigAboutMe = document.querySelector('.big-aboutme');
+
+const tl3 = gsap.timeline({
+  delay: 0,
+  defaults: {
+    duration: 3,
+  },
+});
 
 const tl = gsap.timeline({
-  delay: 0.4,
+  delay: 3,
   defaults: {
     duration: 1,
-    ease: "slow(0.3, 0.4, false)"
-  }
+    ease: 'slow(0.3, 0.4, false)',
+  },
 });
 
 const tl2 = gsap.timeline({
-  duration: 2,
-  ease: "slow(0.1, 0.4, false)",
-  y: -500
+  duration: 4,
+  ease: 'slow(0.1, 0.4, false)',
+  y: -500,
 });
 
+tl3.from(cover2, { visibility: 'visible' });
+tl3.to(cover2, { visibility: 'hidden' });
 tl.to(cover, { height: 0 });
 tl.from(logo, 0.1, { bottom: -10, opacity: 0 });
 tl.to(logo, 0.1, { bottom: 0, opacity: 1 });
